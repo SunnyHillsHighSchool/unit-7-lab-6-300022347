b1263ed46afcd62a439b8da79859ada6ef7d3f46
Kyle Kim <300022347@fjuhsd.org>
2021-02-25 00:58:36 +0000

    Completed unit 7 lab 6

--- Word.java
+++ Word.java
@@ -3,6 +3,7 @@
 //Name -
 
 import static java.lang.System.*;
+import java.util.ArrayList;
 
 public class Word
 {
@@ -10,12 +11,69 @@ public class Word
    																//why is it static?
    
    //add an instance variable
+  private String beginString;
+	//create a 2 constructors
+  public Word()
+  {
+    beginString="";
+  }
 
-	//create a constructor
+  public Word(String initString)
+  {
+    beginString = initString;
+  }
 	
-	//create other methods
-	//look at the sample runner code
-	//on the lab handout
+  //setter method
+  public void setWord(String s)
+    {
+      beginString = s;
+    }
+    
+  //getter method
+  public String getWord()
+  {
+    return beginString;
+  }
 
-	//create a toString method
+  //getNumVowels
+  public int getNumVowels()
+  {
+    ArrayList<String> letterList = new ArrayList<String>();
+    ArrayList<String> vowelList = new ArrayList<String>();
+    int vowelCount=0;
+
+    for(int i=0; i<vowels.length()-1; i++)
+    {
+      vowelList.add( vowels.substring(i,(i+1))   );
+    }
+
+    for(int n=0; n<beginString.length(); n++)
+    {
+      String currentLetter = beginString.substring(n,(n+1));
+      for(int a=0; a<vowelList.size()-1; a++)
+      {
+        if(currentLetter.equals(vowelList.get(a)))
+          vowelCount++;
+        else
+          vowelCount +=0;
+      }
+    }
+
+    return vowelCount;
+  }
+
+  //getLength
+
+  public int getLength()
+  {
+    int lengthOfBeginString = 0;
+    lengthOfBeginString = beginString.length();
+    return lengthOfBeginString;
+  }
+	//create a toString method (Private Instance Variables)
+  public String toString()
+  {
+    return beginString;
+  }
+  
 }
